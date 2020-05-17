c553c4853f465fbe7e4f1e985ae28796af277d99
Leandro.DS <dse.leandro@gmail.com>
2020-05-17 15:35:18 -0300

    Added a date picker

--- src/main/java/com/leandro/consumingrest/controller/PictureController.java
+++ src/main/java/com/leandro/consumingrest/controller/PictureController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,11 +16,13 @@ public class PictureController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping
-    public String getPicture(Model model) {
+    //it makes a request with a specific date only if it has a date param
+    @GetMapping("/")
+    public String getPicture(@RequestParam(name = "date", required = false) String date, Model model) {
         try {
 
-            Picture picture = restTemplate.getForObject("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY",
+            Picture picture = restTemplate.getForObject(
+                "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY" + (date == null? "" : "&date=" + date),
                     Picture.class);
 
             model.addAttribute("data", picture);
